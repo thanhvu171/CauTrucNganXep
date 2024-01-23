ce07f20dacc17e096216642738675a3e6f6116af
NGUYEN DOAN THANH VU <nguyendoanthanhvu1710@gmail.com>
2024-01-23 20:42:18 +0700

    Add project files.

---
+++ CauTrucNganXep/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauTrucNganXep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x;
+            Console.WriteLine("Nhap co so he so 10: ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap co so can doi: ");
+            x = int.Parse(Console.ReadLine());
+
+            Stack A = new Stack();
+            while (n > 0)
+            {
+                A.push(n % x);
+                n = n / x;
+            }
+            int giaTri;
+            Console.WriteLine($"Bieu dien he {x}: ");
+            while (!A.isEmpty())
+            {
+                if (x == 16)
+                {
+                    giaTri = A.pop();
+                    switch (giaTri)
+                    {
+                        case 10:
+                            Console.WriteLine("A");
+                            break;
+                        case 11:
+                            Console.WriteLine("B");
+                            break;
+                        case 12:
+                            Console.WriteLine("C");
+                            break;
+                        case 13:
+                            Console.WriteLine("D");
+                            break;
+                        case 14:
+                            Console.WriteLine("E");
+                            break;
+                        case 15:
+                            Console.WriteLine("F");
+                            break;
+                        default:
+                            Console.WriteLine(giaTri);
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine(giaTri);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CauTrucNganXep/Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauTrucNganXep
+{
+    class Stack
+    {
+        private int[] data;
+        private int top;
+
+        public Stack()
+        {
+            data = new int[100];
+            top = -1;
+        }
+        public Stack(int size)
+        {
+            data = new int[size];
+            top = -1;
+        }
+        public bool isEmpty()
+        {
+            bool kq = false;
+            if (top == - 1)
+            {
+                kq = true;
+            }
+            return kq;
+        }
+        public bool isFull()
+        {
+            bool kq = false;
+            if (top == data.Length - 1) 
+            {
+                kq = true;
+            }
+            return kq;
+        }
+        public void push(int x)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack da day. Them that bai!");
+            }
+            else
+            {
+                top++;
+                data[top] = x;
+            }
+        }
+        public int pop()
+        {
+            int kq = 0;
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack da day. Them that bai!");
+            }
+            else
+            {
+                kq = data[top];
+                top--;
+            }
+            return kq;
+        }
+    }
+}
